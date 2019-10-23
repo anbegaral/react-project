06c682446539940b9777652e0b34509b27fa1632
anbegaral <anab.garciauk@gmail.com>
2019-10-23 08:25:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import {MyComponent} from './components/my-component/myComponent'
+
+function App() {
+  return (
+    <MyComponent/>
+  );
+}
+
+export default App;
---
+++ src/components/my-component/myComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './myComponent.scss';
+
+export class MyComponent extends React.Component {
+    render() {
+        return (
+            <div className="myComponent">
+                <h1>Hello world</h1>
+            </div>
+        )
+    }
+}
+
